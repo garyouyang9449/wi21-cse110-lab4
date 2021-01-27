b28b4e85b1a621dd4c195fe8e255e703127222bb
garyouyang9449 <garyouyang9449@gmail.com>
2021-01-26 22:26:59 -0800

    fixed

--- part1/part1-question20.js
+++ part1/part1-question20.js
@@ -3,14 +3,4 @@ const printTime = () => {
     let time = d.toLocaleTimeString();
     console.log(time);
 }
-//setInterval(printTime, 1000);
-
-
-
-function printNums() {
-    console.log(1);
-    setTimeout(function() { console.log(2) }, 1000);
-    setTimeout(function() { console.log(3) }, 0);
-    console.log(4);
-}
-printNums();
+setInterval(printTime, 1000);
